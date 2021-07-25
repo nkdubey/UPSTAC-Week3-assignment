0fbe0375c70ddb173128333de32c98e2e91c6d3b
Naveen Dubey <naveend@drivewithdarby.com>
2021-07-25 19:56:23 +0530

    completed todo task

--- src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
+++ src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
@@ -4,10 +4,8 @@ package org.upgrad.upstac.testrequests.lab;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.config.security.UserLoggedInService;
 import org.upgrad.upstac.exception.AppException;
 import org.upgrad.upstac.testrequests.RequestStatus;
@@ -31,8 +29,6 @@ public class LabRequestController {
     Logger log = LoggerFactory.getLogger(LabRequestController.class);
 
 
-
-
     @Autowired
     private TestRequestUpdateService testRequestUpdateService;
 
@@ -43,37 +39,34 @@ public class LabRequestController {
     private TestRequestFlowService testRequestFlowService;
 
 
-
     @Autowired
     private UserLoggedInService userLoggedInService;
 
 
-
     @GetMapping("/to-be-tested")
     @PreAuthorize("hasAnyRole('TESTER')")
-    public List<TestRequest> getForTests()  {
+    public List<TestRequest> getForTests() {
 
 
-       return testRequestQueryService.findBy(RequestStatus.INITIATED);
-
-
+        return testRequestQueryService.findBy(RequestStatus.INITIATED);
 
 
     }
 
+    /**
+     * Implement This Method
+     * Create an object of User class and store the current logged in user first
+     * Implement this method to return the list of test requests assigned to current tester(make use of the above created User object)
+     * Make use of the findByTester() method from testRequestQueryService class
+     * For reference check the method getForTests() method from LabRequestController class
+     *
+     * @return
+     */
     @GetMapping
     @PreAuthorize("hasAnyRole('TESTER')")
-    public List<TestRequest> getForTester()  {
-
-        // Implement This Method
-
-        // Create an object of User class and store the current logged in user first
-        //Implement this method to return the list of test requests assigned to current tester(make use of the above created User object)
-        //Make use of the findByTester() method from testRequestQueryService class
-        // For reference check the method getForTests() method from LabRequestController class
-
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented"); // replace this line with your code
-
+    public List<TestRequest> getForTester() {
+        User user = userLoggedInService.getLoggedInUser();
+        return testRequestQueryService.findByTester(user);
 
     }
 
@@ -83,31 +76,27 @@ public class LabRequestController {
     public TestRequest assignForLabTest(@PathVariable Long id) {
 
 
+        User tester = userLoggedInService.getLoggedInUser();
 
-        User tester =userLoggedInService.getLoggedInUser();
-
-      return   testRequestUpdateService.assignForLabTest(id,tester);
+        return testRequestUpdateService.assignForLabTest(id, tester);
     }
 
     @PreAuthorize("hasAnyRole('TESTER')")
     @PutMapping("/update/{id}")
-    public TestRequest updateLabTest(@PathVariable Long id,@RequestBody CreateLabResult createLabResult) {
+    public TestRequest updateLabTest(@PathVariable Long id, @RequestBody CreateLabResult createLabResult) {
 
         try {
 
-            User tester=userLoggedInService.getLoggedInUser();
-            return testRequestUpdateService.updateLabTest(id,createLabResult,tester);
+            User tester = userLoggedInService.getLoggedInUser();
+            return testRequestUpdateService.updateLabTest(id, createLabResult, tester);
 
 
         } catch (ConstraintViolationException e) {
             throw asConstraintViolation(e);
-        }catch (AppException e) {
+        } catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
-
-
-
 
 
 }
